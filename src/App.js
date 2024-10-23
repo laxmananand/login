53f0af7fd1c7b0a0c26aa7f721a5e31401d3c3ee
Laxman Anand <laxman7291@gmail.com>
2024-10-23 16:15:35 +0530

    implemented login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="login-container">
+          <h2>Login</h2>
+          <form>
+            <div className="form-group">
+              <input 
+                type="text" 
+                placeholder="Username"
+                className="login-input"
+              />
+            </div>
+            <div className="form-group">
+              <input 
+                type="password" 
+                placeholder="Password"
+                className="login-input"
+              />
+            </div>
+            <button type="submit" className="login-button">
+              Sign In
+            </button>
+          </form>
+        </div>
       </header>
     </div>
   );
